edaf32ee4ebbde299468133019538cffd538f82f
Gabriel Balasz <gabi@bitvice.ro>
2015-09-25 18:25:53 +0300

    Work on new wrapper utility

--- themes/compile_theme.sh
+++ themes/compile_theme.sh
@@ -1,2 +1,6 @@
-sass  $1/sass/theme.scss $1/css/theme.css --load-path ../sass/
+# SASS Compile command
+# sass  $1/sass/theme.scss $1/css/theme.css --load-path ../sass/
+
+# Compass command
+compass compile $1 -e development
 
